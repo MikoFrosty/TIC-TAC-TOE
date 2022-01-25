39c14dcd2ec1d61e3f16562294fa5fa1e5b66c92
brandon <srlguitarist@gmail.com>
2022-01-24 22:08:13 -0500

    Apply major refacoring: Change many hardcoded values for "X" and "O" to variables.
    Get closer to allow for human and computer to alternate going first, as "X". (disable comment on line 57 for testing)

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -5,47 +5,44 @@ let warning = document.querySelector("#warning");
 let gameOverMessage = document.querySelector("#game-over-message");
 let overlay = document.querySelector("#overlay");
 let playerMoves = 0;
+let playerGoesFirst = true;
 overlay.style.display = "none";
 
 // Listen for tile clicks
 tiles.forEach((tile) => {
   tile.addEventListener("click", (event) => {
+    // Show warning if clicked box already has a letter in it
     if (event.currentTarget.textContent) {
       warning.style.display = "inline-block";
     } else {
       let tileNum = event.currentTarget.id.replace("btn", "");
+      let playerLetter = playerGoesFirst ? "X" : "O";
+      let computerLetter = playerGoesFirst ? "O" : "X";
       warning.style.display = "none";
-      gameState[tileNum] = "X";
+      gameState[tileNum] = playerLetter;
       playerMoves++;
+      console.log(playerMoves);
       updateTiles();
       disableGame();
-      if (winCheck("X")) {
+      if (winCheck(playerLetter)) {
         overlay.style.display = "initial";
         gameOverMessage.textContent = "Congratulations, you won!";
         gameOverMessage.style.opacity = "1";
-      } else if (playerMoves > 4) {
+      } else if (gameState.filter(value => value === "X").length > 4) {
         overlay.style.display = "initial";
         gameOverMessage.textContent = "Draw";
         gameOverMessage.style.opacity = "1";
       } else {
         // Timeout simulates computer thinking :)
         setTimeout(() => {
-          computerO();
-          updateTiles();
-          if (winCheck("O")) {
-            overlay.style.display = "initial";
-            gameOverMessage.textContent = "Computer wins";
-            gameOverMessage.style.opacity = "1";
-          } else {
-            enableGame();
-          }
+          computer(computerLetter);
         }, Math.random() * 300 + 1);
       }
     }
   });
 });
 
-// Reset the board
+// Reset button
 reset.addEventListener("click", () => {
   for (let i = 0; i < gameState.length; i++) {
     gameState[i] = "";
@@ -57,6 +54,12 @@ reset.addEventListener("click", () => {
   updateTiles();
   enableGame();
   playerMoves = 0;
+  /* disabled until feature is finished 
+  playerGoesFirst = !playerGoesFirst;
+  if (!playerGoesFirst) {
+      computer("X");
+  }
+  */
 });
 
 // disables all game tiles
@@ -107,34 +110,38 @@ function winCheck(letter) {
 }
 
 // AI
-function computerO() {
+function computer(letter) {
+    let human = letter === "O" ? "X" : "O"; 
   switch (playerMoves) {
+    case 0:
+        gameState[4] = letter;
+        break;
     case 1:
       // Either random corner, or center if available
       if (gameState[4]) {
         switch (randomNumber(1, 4)) {
           case 1:
-            gameState[0] = "O";
+            gameState[0] = letter;
             break;
           case 2:
-            gameState[2] = "O";
+            gameState[2] = letter;
             break;
           case 3:
-            gameState[6] = "O";
+            gameState[6] = letter;
             break;
           case 4:
-            gameState[8] = "O";
+            gameState[8] = letter;
             break;
         }
       } else {
-        gameState[4] = "O";
+        gameState[4] = letter;
       }
       break;
     case 2:
       if (gameState[4] === "X") {
         let nextMoveX = readyToWin("X");
         if (nextMoveX < 9) {
-          gameState[nextMoveX] = "O";
+          gameState[nextMoveX] = letter;
         } else {
           let randomCorner = randomNumber(1, 4) * 2;
           // to make randomCorner 0, 2, 6, or 8
@@ -142,7 +149,7 @@ function computerO() {
             randomCorner = 0;
           }
           if (!gameState[randomCorner]) {
-            gameState[randomCorner] = "O";
+            gameState[randomCorner] = letter;
           } else {
             // if RandomCorner isn't a valid spot, then just iterate through all corners until empty spot is found
             for (let i = 0; i < gameState.length; i += 2) {
@@ -150,7 +157,7 @@ function computerO() {
                 i = 6;
               }
               if (!gameState[i]) {
-                gameState[i] = "O";
+                gameState[i] = letter;
                 break;
               }
             }
@@ -158,73 +165,73 @@ function computerO() {
         }
       }
 
-      // refacored above for more variability, flexibility, and natural like behaivor
+      // refacored above for more variability, flexibility, and natural like behavior
       /*
     // If player chose the center first, then only check edges for next move
       if (gameState[4] === "X") {
         if (gameState[0] || gameState[6]) {
-          gameState[2] = "O";
+          gameState[2] = letter;
         } else if (gameState[7]) {
-          gameState[1] = "O";
+          gameState[1] = letter;
         } else if (gameState[5]) {
-          gameState[3] = "O";
+          gameState[3] = letter;
         } else if (gameState[3]) {
-          gameState[5] = "O";
+          gameState[5] = letter;
         } else if (gameState[2]) {
-          gameState[6] = "O";
+          gameState[6] = letter;
         } else if (gameState[1]) {
-          gameState[7] = "O";
+          gameState[7] = letter;
         } else {
           alert(`gameState error (case 2 if [4])`);
         }
       }*/
       else {
         if (
-          isMatch(gameState[0], gameState[1]) ||
-          isMatch(gameState[5], gameState[8]) ||
-          isMatch(gameState[1], gameState[7]) ||
-          isMatch(gameState[3], gameState[5]) ||
-          isMatch(gameState[0], gameState[5]) ||
-          isMatch(gameState[1], gameState[8]) ||
-          isMatch(gameState[1], gameState[5])
+          isMatch(gameState[0], gameState[1], human) ||
+          isMatch(gameState[5], gameState[8], human) ||
+          isMatch(gameState[1], gameState[7], human) ||
+          isMatch(gameState[3], gameState[5], human) ||
+          isMatch(gameState[0], gameState[5], human) ||
+          isMatch(gameState[1], gameState[8], human) ||
+          isMatch(gameState[1], gameState[5], human)
         ) {
-          gameState[2] = "O";
+          gameState[2] = letter;
         } else if (
-          isMatch(gameState[0], gameState[8]) ||
-          isMatch(gameState[2], gameState[6]) ||
-          isMatch(gameState[2], gameState[8])
+          isMatch(gameState[0], gameState[8], human) ||
+          isMatch(gameState[2], gameState[6], human) ||
+          isMatch(gameState[2], gameState[8], human)
         ) {
-          gameState[5] = "O";
+          gameState[5] = letter;
         } else if (
-          isMatch(gameState[1], gameState[2]) ||
-          isMatch(gameState[3], gameState[6]) ||
-          isMatch(gameState[2], gameState[3]) ||
-          isMatch(gameState[1], gameState[6]) ||
-          isMatch(gameState[1], gameState[3])
+          isMatch(gameState[1], gameState[2], human) ||
+          isMatch(gameState[3], gameState[6], human) ||
+          isMatch(gameState[2], gameState[3], human) ||
+          isMatch(gameState[1], gameState[6], human) ||
+          isMatch(gameState[1], gameState[3], human)
         ) {
-          gameState[0] = "O";
+          gameState[0] = letter;
         } else if (
-          isMatch(gameState[2], gameState[5]) ||
-          isMatch(gameState[6], gameState[7]) ||
-          isMatch(gameState[2], gameState[7]) ||
-          isMatch(gameState[5], gameState[6]) ||
-          isMatch(gameState[5], gameState[7])
+          isMatch(gameState[2], gameState[5], human) ||
+          isMatch(gameState[6], gameState[7], human) ||
+          isMatch(gameState[2], gameState[7], human) ||
+          isMatch(gameState[5], gameState[6], human) ||
+          isMatch(gameState[5], gameState[7], human)
         ) {
-          gameState[8] = "O";
+          gameState[8] = letter;
         } else if (
-          isMatch(gameState[7], gameState[8]) ||
-          isMatch(gameState[0], gameState[3]) ||
-          isMatch(gameState[0], gameState[7]) ||
-          isMatch(gameState[3], gameState[8]) ||
-          isMatch(gameState[3], gameState[7])
+          isMatch(gameState[7], gameState[8], human) ||
+          isMatch(gameState[0], gameState[3], human) ||
+          isMatch(gameState[0], gameState[7], human) ||
+          isMatch(gameState[3], gameState[8], human) ||
+          isMatch(gameState[3], gameState[7], human)
         ) {
-          gameState[6] = "O";
-        } else if (isMatch(gameState[0], gameState[2])) {
-          gameState[1] = "O";
-        } else if (isMatch(gameState[0], gameState[6])) {
-          gameState[3] = "O";
-        } else if (isMatch(gameState[6], gameState[8])) {
-          gameState[7] = "O";
+          gameState[6] = letter;
+        } else if (isMatch(gameState[0], gameState[2], human)) {
+          gameState[1] = letter;
+        } else if (isMatch(gameState[0], gameState[6], human)) {
+          gameState[3] = letter;
+        } else if (isMatch(gameState[6], gameState[8], human)) {
+          gameState[7] = letter;
         } else {
           alert("gameState error (case 2 if ![4])");
         }
@@ -232,34 +239,34 @@ function computerO() {
       break;
     case 3:
     case 4: {
-      let nextMoveO = readyToWin("O"),
-        nextMoveX = readyToWin("X");
+      let nextMoveO = readyToWin(letter),
+        nextMoveX = readyToWin(human);
 
       if (nextMoveO < 9) {
-        gameState[nextMoveO] = "O";
+        gameState[nextMoveO] = letter;
       } else if (nextMoveX < 9) {
-        gameState[nextMoveX] = "O";
+        gameState[nextMoveX] = letter;
       } else if (nextMoveO + nextMoveX === 18) {
         for (let i = 0; i < gameState.length; i++) {
           // Fix for specific cases: X| |     | |X    |X|
           //                          |O|X   X|O|O    |O|
           //                          |X|O    | |X   X|O|X
           if (
-            (isMatch(gameState[5], gameState[7], "X") &&
-              gameState[0] === "X" &&
+            (isMatch(gameState[5], gameState[7], human) &&
+              gameState[0] === human &&
               i === 1) ||
-            (isMatch(gameState[2], gameState[8], "X") &&
-              gameState[3] === "X" &&
+            (isMatch(gameState[2], gameState[8], human) &&
+              gameState[3] === human &&
               i === 0) ||
-            (isMatch(gameState[6], gameState[8], "X") &&
-              gameState[1] === "X" &&
+            (isMatch(gameState[6], gameState[8], human) &&
+              gameState[1] === human &&
               (i === 0 || i === 2))
           ) {
             i++;
           }
           // Place O on first empty tile found
           if (!gameState[i]) {
-            gameState[i] = "O";
+            gameState[i] = letter;
             break;
           }
         }
@@ -271,65 +278,77 @@ function computerO() {
       break;
     }
   }
+
+  updateTiles();
+  if (winCheck(letter)) {
+    overlay.style.display = "initial";
+    gameOverMessage.textContent = "Computer wins";
+    gameOverMessage.style.opacity = "1";
+  } else {
+    enableGame();
+  }
+
   // Checks if two tiles are a matching for a given letter
-  function isMatch(position1, position2, letter = "X") {
-    if (position1 === letter && position2 === letter) {
+  function isMatch(position1, position2, currentLetter = "") {
+    if (currentLetter === "") {
+        alert("isMatch error: currentLetter not defined");
+    } else if (position1 === currentLetter && position2 === currentLetter) {
       return true;
-    }
+    } 
     return false;
   }
 
   // Evaluate all positions for given letter to determine if there is a winning chance.
   // Return next suggested move, or 9 (if no winning position is found)
   // 9 is returned instead of false because false would conflict with gameState[0]
-  function readyToWin(letter) {
-    if (isMatch(gameState[0], gameState[1], letter) && !gameState[2]) {
+  function readyToWin(currentLetter) {
+    if (isMatch(gameState[0], gameState[1], currentLetter) && !gameState[2]) {
       return 2;
-    } else if (isMatch(gameState[0], gameState[2], letter) && !gameState[1]) {
+    } else if (isMatch(gameState[0], gameState[2], currentLetter) && !gameState[1]) {
       return 1;
-    } else if (isMatch(gameState[1], gameState[2], letter) && !gameState[0]) {
+    } else if (isMatch(gameState[1], gameState[2], currentLetter) && !gameState[0]) {
       return 0;
-    } else if (isMatch(gameState[3], gameState[4], letter) && !gameState[5]) {
+    } else if (isMatch(gameState[3], gameState[4], currentLetter) && !gameState[5]) {
       return 5;
-    } else if (isMatch(gameState[3], gameState[5], letter) && !gameState[4]) {
+    } else if (isMatch(gameState[3], gameState[5], currentLetter) && !gameState[4]) {
       return 4;
-    } else if (isMatch(gameState[4], gameState[5], letter) && !gameState[3]) {
+    } else if (isMatch(gameState[4], gameState[5], currentLetter) && !gameState[3]) {
       return 3;
-    } else if (isMatch(gameState[6], gameState[7], letter) && !gameState[8]) {
+    } else if (isMatch(gameState[6], gameState[7], currentLetter) && !gameState[8]) {
       return 8;
-    } else if (isMatch(gameState[6], gameState[8], letter) && !gameState[7]) {
+    } else if (isMatch(gameState[6], gameState[8], currentLetter) && !gameState[7]) {
       return 7;
-    } else if (isMatch(gameState[7], gameState[8], letter) && !gameState[6]) {
+    } else if (isMatch(gameState[7], gameState[8], currentLetter) && !gameState[6]) {
       return 6;
-    } else if (isMatch(gameState[0], gameState[3], letter) && !gameState[6]) {
+    } else if (isMatch(gameState[0], gameState[3], currentLetter) && !gameState[6]) {
       return 6;
-    } else if (isMatch(gameState[0], gameState[6], letter) && !gameState[3]) {
+    } else if (isMatch(gameState[0], gameState[6], currentLetter) && !gameState[3]) {
       return 3;
-    } else if (isMatch(gameState[3], gameState[6], letter) && !gameState[0]) {
+    } else if (isMatch(gameState[3], gameState[6], currentLetter) && !gameState[0]) {
       return 0;
-    } else if (isMatch(gameState[1], gameState[4], letter) && !gameState[7]) {
+    } else if (isMatch(gameState[1], gameState[4], currentLetter) && !gameState[7]) {
       return 7;
-    } else if (isMatch(gameState[1], gameState[7], letter) && !gameState[4]) {
+    } else if (isMatch(gameState[1], gameState[7], currentLetter) && !gameState[4]) {
       return 4;
-    } else if (isMatch(gameState[4], gameState[7], letter) && !gameState[1]) {
+    } else if (isMatch(gameState[4], gameState[7], currentLetter) && !gameState[1]) {
       return 1;
-    } else if (isMatch(gameState[2], gameState[5], letter) && !gameState[8]) {
+    } else if (isMatch(gameState[2], gameState[5], currentLetter) && !gameState[8]) {
       return 8;
-    } else if (isMatch(gameState[2], gameState[8], letter) && !gameState[5]) {
+    } else if (isMatch(gameState[2], gameState[8], currentLetter) && !gameState[5]) {
       return 5;
-    } else if (isMatch(gameState[5], gameState[8], letter) && !gameState[2]) {
+    } else if (isMatch(gameState[5], gameState[8], currentLetter) && !gameState[2]) {
       return 2;
-    } else if (isMatch(gameState[0], gameState[4], letter) && !gameState[8]) {
+    } else if (isMatch(gameState[0], gameState[4], currentLetter) && !gameState[8]) {
       return 8;
-    } else if (isMatch(gameState[0], gameState[8], letter) && !gameState[4]) {
+    } else if (isMatch(gameState[0], gameState[8], currentLetter) && !gameState[4]) {
       return 4;
-    } else if (isMatch(gameState[4], gameState[8], letter) && !gameState[0]) {
+    } else if (isMatch(gameState[4], gameState[8], currentLetter) && !gameState[0]) {
       return 0;
-    } else if (isMatch(gameState[2], gameState[4], letter) && !gameState[6]) {
+    } else if (isMatch(gameState[2], gameState[4], currentLetter) && !gameState[6]) {
       return 6;
-    } else if (isMatch(gameState[2], gameState[6], letter) && !gameState[4]) {
+    } else if (isMatch(gameState[2], gameState[6], currentLetter) && !gameState[4]) {
       return 4;
-    } else if (isMatch(gameState[4], gameState[6], letter) && !gameState[2]) {
+    } else if (isMatch(gameState[4], gameState[6], currentLetter) && !gameState[2]) {
       return 2;
     }
     return 9;
